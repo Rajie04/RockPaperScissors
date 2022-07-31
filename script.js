86c3a27b1360a7f3ed7b4487d790cbead2d46fdf
Rajie04 <rtahmed04@gmail.com>
2022-07-30 21:10:21 -0500

    make playerSelection and computerSelection

---
+++ script.js
@@ -0,0 +1,28 @@
+let choice = ['Rock', 'Paper', 'Scissors'];
+
+function getComputerChoice(choice) {
+  return choice[Math.floor(Math.random()*choice.length)];
+}
+console.log(getComputerChoice(choice));
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === computerSelection){
+      return `It's a tie! You both picked ${playerSelection}`;
+    } else if(playerSelection === "Rock" && computerSelection === "Paper"){
+       return "You lose! Paper beats Rock";
+    } else if(playerSelection === "Rock" && computerSelection === "Scissors"){
+      return "You win ! Rock beats Scissors";
+    } else if(playerSelection === "Paper" && computerSelection === "Rock"){
+      return "You win ! Paper beats Rock";
+    } else if(playerSelection === "Paper" && computerSelection === "Scissors"){
+      return "You lose ! Scissors beats Paper";
+    } else if(playerSelection === "Scissors" && computerSelection === "Paper"){
+      return "You win ! Scissors beats Paper";
+    } else if(playerSelection === "Scissors" && computerSelection === "Rock"){
+      return "You lose ! Rock beats Scissors";
+    }
+}
+ 
+const playerSelection = prompt("Choose Your Weapon !");
+const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
